09c8f1396acad7d66707cc193a27384242c2b061
easy-min <100506494+easy-min@users.noreply.github.com>
2025-03-24 16:13:10 +0900

    Create baekjoon15988123더하기3.java

---
+++ APS/recursiveCall/baekjoon15988123더하기3.java
@@ -0,0 +1,30 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon15988123더하기3 {
+	static int[] dp = new int[1000001];
+	static int MOD = 1000000009;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		makeDP();
+		for (int test = 1; test<=T; test++) {
+			int N = Integer.parseInt(br.readLine());
+			System.out.println(dp[N]);
+			
+		}
+
+	}
+	static void makeDP() {
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		for (int i = 4; i<1000001; i++) {
+			dp[i] = ((dp[i-1] + dp[i-2])%MOD + dp[i-3])%MOD;
+		}
+	}
+
+}
